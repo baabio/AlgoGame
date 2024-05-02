f85a9ebb949745b1d8bd2e86a938a9601a0ee30e
Babacar MBENGUE <mbengue-babacar@hotmail.com>
2024-05-02 11:42:35 +0200

    la descente

--- algo.js
+++ algo.js
@@ -19,4 +19,20 @@ while (true) {
 }
 
 // Puzzle 2
+// game loop
+while (true) {
+    let max = 0;
+    let imax = 0;
+    for (let i = 0; i < 8; i++) {
+        const mountainH = parseInt(readline()); // represents the height of one mountain.
+        if (mountainH > max){
+            max = mountainH
+            imax = i
+        }
+    }
+    
+    // Write an action using console.log()
+    // To debug: console.error('Debug messages...');
 
+    console.log(imax);     // The index of the mountain to fire on.
+}
